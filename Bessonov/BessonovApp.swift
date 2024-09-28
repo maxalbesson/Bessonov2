2d862101358468bcdc5d47066a4886ae9d33528b
nke <nke.wsr.309.07@gmail.com>
2024-09-28 09:17:37 +0700

    Initial Commit

---
+++ Bessonov/BessonovApp.swift
@@ -0,0 +1,17 @@
+//
+//  BessonovApp.swift
+//  Bessonov
+//
+//  Created by user on 28.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BessonovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
